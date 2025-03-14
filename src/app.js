3233701b67f7410da9f6553e66cff0917f218f30
Pabloherresp <pabloherreraespinola@gmail.com>
2025-03-14 19:39:57 +0000

    First solution

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  //declaración de datos del dominio
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon','sides','sitcom', 'audio'];
+  let dom = ['.es','.com','.us','.io']
+
+
+  //función que realiza la acción principal
+  const printResult = (i,j,k,m) => {
+    let result = ""
+
+    //condicional para "domain hacks"
+    if(k == "sides" && m == ".es"){
+      result = i+j+"sid"+m
+    } else if(k == "sitcom" && m == ".com"){
+      result = i+j+"sit"+m
+    } else if(k == "audio" && m == ".io"){
+      result = i+j+"aud"+m
+    }
+    else{
+      result = i+j+k+m
+    }
+
+    //mostrar en consola
+    console.log(result)
+
+    //mostrar en HTML
+    document.getElementById("dom-" + m.substring(1)).innerHTML += `<p class="text-center">${result}</p>`
+  }
+
+  //pronoun.forEach(i => adj.forEach(j => noun.forEach(k => dom.forEach(m => printResult(i,j,k,m)))))
+
+  //loop de los elementos
+  for(let i in pronoun)
+    for(let j in adj)
+      for(let k in noun)
+        for(let m in dom)
+          printResult(pronoun[i],adj[j],noun[k],dom[m])
+
+
   console.log("Hello Rigo from the console!");
 };
